bcc58f6e2c72054962649f1b4a1c50ef1d83bac6
Ho-Wan To <ho.wan2345@gmail.com>
2021-12-12 22:59:04 +0000

    show status bar items

--- src/extension.ts
+++ src/extension.ts
@@ -2,39 +2,73 @@
 
 import * as vscode from "vscode";
 // name of settings strings
-const primaryToggle: string = "toggle.settingTitle";
-const toggleTitle_s1: string = "toggle.setting1Title";
-const toggleTitle_s2: string = "toggle.setting2Title";
+const g_togglePrimary: string = "toggle.settingTitle";
+const g_toggleTitle_s1: string = "toggle.setting1Title";
+const g_toggleTitle_s2: string = "toggle.setting2Title";
 // name of setting states and default values specified in package.json
 const g_settingState1: string = "toggle.settingState1";
 const g_settingState2: string = "toggle.settingState2";
 const g_state1Default: string = "state1";
 const g_state2Default: string = "state2";
+// name of commands
+const g_commandToggleP = "extension.toggle";
+const g_commandToggleS1 = "extension.toggle_s1";
+const g_commandToggleS2 = "extension.toggle_s2";
+// status bar items
+let g_statusBarTogP: vscode.StatusBarItem;
+let g_statusBarTogS1: vscode.StatusBarItem;
+let g_statusBarTogS2: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(`Extension "toggle-btn" is now active!`);
+  console.log(`Extension "toggle-btn" is now active! again!`);
   // Setting Toggle (primary)
-  let disposable = vscode.commands.registerCommand("extension.toggle", () => {
-    toggleSetting(primaryToggle);
+  const disposable = vscode.commands.registerCommand(g_commandToggleP, () => {
+    toggleSetting(g_togglePrimary);
   });
+
+  g_statusBarTogP = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    3
+  );
+  g_statusBarTogP.text = "T-P";
+  g_statusBarTogP.command = g_commandToggleP;
+  context.subscriptions.push(g_statusBarTogP);
+
   // Setting 1 Toggle
-  let disposable1 = vscode.commands.registerCommand(
-    "extension.toggle_s1",
-    () => {
-      toggleSetting(toggleTitle_s1);
-    }
+  const disposable1 = vscode.commands.registerCommand(g_commandToggleS1, () => {
+    toggleSetting(g_toggleTitle_s1);
+  });
+  g_statusBarTogS1 = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    2
   );
+  g_statusBarTogS1.text = "T-S1";
+  g_statusBarTogS1.command = g_commandToggleS1;
+  context.subscriptions.push(g_statusBarTogS1);
+
   // Setting 2 Toggle
-  let disposable2 = vscode.commands.registerCommand(
-    "extension.toggle_s2",
-    () => {
-      toggleSetting(toggleTitle_s2);
-    }
+  const disposable2 = vscode.commands.registerCommand(g_commandToggleS2, () => {
+    toggleSetting(g_toggleTitle_s2);
+  });
+  g_statusBarTogS2 = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    1
   );
+  g_statusBarTogS2.text = "T-S2";
+  g_statusBarTogS2.command = g_commandToggleS2;
+  context.subscriptions.push(g_statusBarTogS2);
 
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable1);
   context.subscriptions.push(disposable2);
+
+  showStatusBarItem();
+}
+
+function showStatusBarItem(): void {
+  g_statusBarTogP.show();
+  g_statusBarTogS1.show();
+  g_statusBarTogS2.show();
 }
 
 export function deactivate() {}
