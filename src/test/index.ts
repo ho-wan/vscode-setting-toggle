a6e3ca651c67b79e3b0fc01aba9a9230839d102b
ho-wan <howan.eng@gmail.com>
2026-05-02 21:15:23 +0100

    chore: update dependencies and improve test runner
    - Bump VSCode engine requirement to ^1.118.0 in package.json.
    - Update devDependencies:
      - @types/mocha to ^10.0.10
      - @types/node to ^20.0.0
      - @types/vscode to ^1.118.0
      - @vscode/test-electron to ^2.5.2
      - typescript to ^6.0.3
    - Refactor test runner in src/test/index.ts to use Mocha directly, improving error handling and test execution.

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,22 +1,21 @@
-//
-// PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
-//
-// This file is providing the test runner to use when running extension tests.
-// By default the test runner in use is Mocha based.
-//
-// You can provide your own test runner if you want to override it by exporting
-// a function run(testRoot: string, clb: (error:Error) => void) that the extension
-// host can call to run the tests. The test runner is expected to use console.log
-// to report the results back to the caller. When the tests are finished, return
-// a possible error to the callback or null if none.
+import * as path from 'path';
+import Mocha from 'mocha';
 
-import * as testRunner from 'vscode/lib/testrunner';
+export function run(): Promise<void> {
+    const mocha = new Mocha({ ui: 'tdd', color: true });
+    const testsRoot = path.resolve(__dirname, '.');
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
-
-module.exports = testRunner;
+    return new Promise((resolve, reject) => {
+        try {
+            mocha.run(failures => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`));
+                } else {
+                    resolve();
+                }
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
